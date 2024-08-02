67e420db77b80f64ff7fe1e12e9829af4bd8ae52
FrancescoCesari97 <freedus97@gmail.com>
2024-08-02 10:58:18 +0200

    added moving object inside the level class

--- code/level.py
+++ code/level.py
@@ -15,12 +15,28 @@ class Level:
         self.setup(tmx_map)
     
     def setup(self, tmx_map):
+        # * tiles
         for x, y, surf, in tmx_map.get_layer_by_name('Terrain').tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites))
 
+        # * objects
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
                 PLayer((obj.x, obj.y), surf, self.all_sprites, self.collision_sprites)
+
+        # * moving objects
+        for obj in tmx_map.get_layer_by_name('Moving Objects'):
+            if obj.name == 'helicopter':
+                if obj.width > obj.height: #*-> horizontal movement
+                    move_dir = 'x'
+                    start_pos = (obj.x, obj.y + obj.height / 2)
+                    end_pos = (obj.x + obj.width, obj.y + obj.height / 2)
+                else: #*-> vertical movement
+                    move_dir = 'y'
+                    start_pos = (obj.x + obj.width / 2, obj.y)
+                    end_pos = (obj.x + obj.width / 2, obj.y + obj.height)
+                speed = obj.properties['speed']
+                
 
     def run(self, dt):
         self.display_surface.fill('black')
