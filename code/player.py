95d305b1b6eb419ae4eda7280d129114f50c2ac4
FrancescoCesari97 <freedus97@gmail.com>
2024-08-05 12:00:16 +0200

    added coreection in the semi_collision method

--- code/player.py
+++ code/player.py
@@ -144,7 +144,8 @@ class PLayer(pygame.sprite.Sprite):
                 if sprite.rect.colliderect(self.rect):
                     if self.rect.bottom >= sprite.rect.top and int(self.old_rect.bottom) <= sprite.old_rect.top:
                         self.rect.bottom = sprite.rect.top
-                        self.direction.y = 0
+                        if self.direction.y > 0:
+                            self.direction.y = 0
 
     def update_timers(self):
         for timer in self.timers.values():
