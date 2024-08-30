3a1f6fed16526f33cb5e710914c888c58e5ddb4f
FrancescoCesari97 <freedus97@gmail.com>
2024-08-30 18:01:47 +0200

    added movement to the spike

--- code/sprites.py
+++ code/sprites.py
@@ -84,12 +84,26 @@ class Spike(Sprite):
         self.start_angle = start_angle
         self.end_angle = end_angle
         self.angle = self.start_angle
+        self.direction = 1
+        self.full_circle = True if self.end_angle == -1 else False
 
         # * trigonometry
         x = self.center[0] + cos(radians(self.angle)) * self.radius
         y = self.center[1] + sin(radians(self.angle)) * self.radius
+        
 
         super().__init__((x, y), surf, groups, z)
 
     def update(self, dt):
-        self.angle += self.speed * dt
+        self.angle += self.direction * self.speed * dt
+
+
+        if not self.full_circle:
+            if self.angle >= self.end_angle:
+                self.direction = -1
+            if self.angle < self.start_angle:
+                self.direction = 1
+
+        x = self.center[0] + cos(radians(self.angle)) * self.radius
+        y = self.center[1] + sin(radians(self.angle)) * self.radius
+        self.rect.center = (x, y)
